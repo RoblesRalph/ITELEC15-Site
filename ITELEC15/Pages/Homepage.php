9935e206b8e3b29487b04e27ceb8eadf813e1cb1
RoblesRalph <61932828+RoblesRalph@users.noreply.github.com>
2021-02-20 15:00:22 +0800

    Add files via upload

---
+++ ITELEC15/Pages/Homepage.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ralph's Homepage</title>
+	<link rel="stylesheet" type="text/css" href="../CSS/stylesheet.css">
+	<link href='https://fonts.googleapis.com/css?family=Play' rel='stylesheet'>
+	<link href='https://fonts.googleapis.com/css?family=Goldman' rel='stylesheet'>
+</head>
+<body>
+	<div class="headbar">
+		<a href="Homepage.php" style="float: left;" class="barlinks"><h2>Ralph Robles</h2></a>
+	</div>
+	<div class="navbar">
+		<div class="baroptions">
+			<a href="About.php" class="barlinks"><b>About</b></a> |
+			<a href="Profile.php" class="barlinks"><b>Profile</b></a> |
+			<a href="Gallery.php" class="barlinks"><b>Gallery</b></a> |
+			<a href="Trivia.php" class="barlinks"><b>Trivia</b></a>
+		</div>
+	</div>
+	<br>
+	<br>
+	<div class="body_div">
+		<h1>Welcome!</h1>
+		<br>
+		<br>
+		<h4>My name is Ralph Robles, and I welcome you to my website.</h4>
+		<br>
+		<br>
+		<h4>If you wish to learn more about me, click on any of the options on the navigation bar.</h4>
+		<br>
+		<br>
+		<h4>Have a nice day!</h4>
+	</div>
+</body>
+</html>
---
+++ ITELEC15/Pages/Trivia.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ralph's Trivia</title>
+	<link rel="stylesheet" type="text/css" href="../CSS/stylesheet.css">
+	<link href='https://fonts.googleapis.com/css?family=Play' rel='stylesheet'>
+	<link href='https://fonts.googleapis.com/css?family=Goldman' rel='stylesheet'>
+</head>
+<body>
+	<div class="headbar">
+		<a href="Homepage.php" style="float: left;" class="barlinks"><h2>Ralph Robles</h2></a>
+	</div>
+	<div class="navbar">
+		<div class="baroptions">
+			<a href="About.php" class="barlinks"><b>About</b></a> |
+			<a href="Profile.php" class="barlinks"><b>Profile</b></a> |
+			<a href="Gallery.php" class="barlinks"><b>Gallery</b></a> |
+			<a href="Trivia.php" class="barlinks"><b>Trivia</b></a>
+		</div>
+	</div>
+	<br>
+	<br>
+	<div class="body_div">
+		<h1>Some Trivia about myself: </h1>
+		<br>
+		<br>
+		<br>
+		<br>
+		<p>- I'm a big fan of Nintendo products</p>
+		<br>
+		<p>- Fanfiction is my favorite thing to read</p>
+		<br>
+		<p>- I've watched every Pixar movie</p>
+		<br>
+		<p>- I graduated second honors in pre-school</p>
+		<br>
+		<p>- I was born on Halloween</p>
+		<br>
+		<p>- I worked in Virlanie during my Senior High job training</p>
+	</div>
+</body>
+</html>
